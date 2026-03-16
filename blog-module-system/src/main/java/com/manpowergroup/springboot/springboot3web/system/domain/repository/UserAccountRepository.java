879f58b9c3445a345d6c89541fca31b43b7ddb03
Yao Xia <yx18627792049@gmail.com>
2026-03-16 22:24:07 +0900

    docs: UserAccountRepositoryにJavadocコメントを追加 (#5)
    Repository層のメソッドに説明およびパラメータコメントを追加し、
    DDDアーキテクチャの可読性を改善。
    
    docs: 为UserAccountRepository补充Javadoc注释 (#5)
    
    为Repository层方法补充说明与参数注释，提升DDD架构代码可读性。

--- blog-module-system/src/main/java/com/manpowergroup/springboot/springboot3web/system/domain/repository/UserAccountRepository.java
+++ blog-module-system/src/main/java/com/manpowergroup/springboot/springboot3web/system/domain/repository/UserAccountRepository.java
@@ -6,9 +6,10 @@ public interface UserAccountRepository {
     /**
      * アカウント種別とアカウント値によりログインユーザー情報を取得する
      *
+     *
      * @param accountType アカウント種別
      * @param accountValue アカウント値
      * @return ログインユーザー情報
      */
-    LoginAccountUserDTO findLoginUserByAccountTypeAndAccountValue(String accountType, String accountValue);
+    LoginAccountUserDTO findLoginUserByAccountTypeAndAccountValue(String accountType,String accountValue);
 }
